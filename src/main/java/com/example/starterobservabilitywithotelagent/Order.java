d37cc35a45fcdfb9363a1f694d6110f70b2385bb
esteban_89_1 <esteban_89_1@hotmail.com>
2023-12-28 02:54:28 -0300

    initial commit

---
+++ src/main/java/com/example/starterobservabilitywithotelagent/Order.java
@@ -0,0 +1,26 @@
+package com.example.starterobservabilitywithotelagent;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor
+public class Order {
+    @Id
+    Long id;
+    @Column(name = "customer_id")
+    Long customerId;
+    @Column(name = "order_date")
+    ZonedDateTime orderDate;
+    @Column(name = "total_amount")
+    BigDecimal totalAmount;
+}
